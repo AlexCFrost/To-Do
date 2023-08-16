5b7c0b1fac5ae5e04e9a559046ee8a94c3a8af28
AlexCFrost <siva.ganesh.c18@gmail.com>
2023-08-16 15:47:46 +0530

    Created an app for To-Do list

---
+++ app.js
@@ -0,0 +1,49 @@
+let button = document.querySelector("#button")
+
+let todolist = document.getElementById("todolist")
+
+let input = document.getElementById("input")
+
+let arr = []
+
+button.onclick = function(){
+
+    arr.push(input.value)
+    input.value = ""
+    console.log(arr)
+    appendList()
+}
+
+function appendList(){
+todolist.innerHTML = null
+    for(let i=0;i<arr.length;i++){
+
+        // let new_list = document.createElement("li")
+        // new_list.innerText = arr[i]
+        // toDOList.append(new_list)
+
+        // todolist.innerHTML += "<li>" + arr[i] + "</li>"
+        todolist.innerHTML += `<li> ${arr[i]} <a onclick = editlist(${i}),deletelist(${i})>Edit</a></li>`
+        todolist.innerHTML += `<li> ${arr[i]} <a onclick = deletelist(${i})>x</a> </li>`
+    }
+}
+
+// Edit functionality
+
+function editlist(index){
+    let editedText = prompt("The New Title")
+    arr[index] = editedText
+    appendList()
+}
+
+//delete functionality
+
+function deletelist(index){
+    let Conformdeleted = confirm("Are you sure?")
+    if (Conformdeleted) {
+        arr.splice(index,1)
+        appendList()
+    }
+    
+}
+
